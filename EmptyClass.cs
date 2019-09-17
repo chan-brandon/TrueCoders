372bed6aae479d6bea321f71d124c367a2c753a2
Brandon Chan <brandonhofaichan@gmail.com>
2019-09-17 13:16:49 -0500

    added files to in class assignment on Classes

---
+++ EmptyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TrueCoders
+{
+    public class Gamers
+    {
+        public string Name { get; set; }
+        public decimal Winnings { get; set; }
+        public string Team { get; set; }
+        public int Rank { get; set; }
+
+        public Gamers(string nickname, decimal winnings, string team, int rank)
+        {
+            Name = nickname;
+            Winnings = winnings;
+            Team = team;
+            Rank = rank;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TrueCoders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gamers simple = new Gamers("s1mple", 510, "NaVi", 1);
+            Gamers fallen = new Gamers("Fallen", 957, "MiBR", 6);
+            Gamers device = new Gamers("dev1ce", 1500, "Astralis", 3);
+            Gamers zywoo = new Gamers("ZywOo", 157, "Vitality", 2);
+            Gamers coldzera = new Gamers("coldzera", 924, "FaZe Clan", 4);
+            Gamers niko = new Gamers("NiKo", 657, "FaZe Clan", 5);
+
+            var playerList = new List<Gamers>();
+            playerList.Add(simple);
+            playerList.Add(zywoo);
+            playerList.Add(device);
+            playerList.Add(coldzera);
+            playerList.Add(niko);
+            playerList.Add(fallen);
+
+            foreach (Gamers player in playerList)
+            {
+                Console.WriteLine($"{player.Name} is the #{player.Rank} player in the world that plays for {player.Team} and has earned ${player.Winnings}k in his career.");
+            }
+        }
+    }
+}
